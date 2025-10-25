6455e8f532fdac304038f9333b8fb1b367bc2471
JL <johnliveeoroncillo@gmail.com>
2025-10-25 13:07:41 +0800

    Update prisma.ts

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,5 @@
-import { PrismaClient } from '@prisma/client';
+const { PrismaClient } = require('@prisma/client');
+import { omit } from '../utils/object.utils';
 
 declare global {
     // eslint-disable-next-line no-var
@@ -6,24 +7,31 @@ declare global {
 }
 
 function createPrismaClient() {
-    return new PrismaClient().$extends({
+    const baseClient = new PrismaClient();
+    
+    const extendedClient = baseClient.$extends({
         query: {
             $allModels: {
-                async $allOperations({ operation, args, query }) {
+                async $allOperations({ operation, args, query }: { operation: string; args: any; query: any }) {
                     // Only apply to operations that support where clause
                     if (['findMany', 'findFirst', 'findUnique', 'count'].includes(operation)) {
                         const argsWithWhere = args as any;
-                        if (argsWithWhere.where) {
-                            argsWithWhere.where.deletedAt = null;
-                        } else {
-                            argsWithWhere.where = { deletedAt: null };
+                        // Only add deletedAt filter if includeDeleted is not true
+                        if (!argsWithWhere.includeDeleted) {
+                            if (argsWithWhere.where) {
+                                argsWithWhere.where.deletedAt = null;
+                            } else {
+                                argsWithWhere.where = { deletedAt: null };
+                            }
                         }
                     }
-                    return query(args);
+                    return query(omit(args, ['includeDeleted']));
                 },
             },
         },
     });
+
+    return extendedClient;
 }
 
 // Prevent multiple instances of Prisma Client in development
