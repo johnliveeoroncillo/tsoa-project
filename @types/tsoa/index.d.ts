cb266531214032cd8423243b0ff585ee60805854
JL <johnliveeoroncillo@gmail.com>
2026-01-03 18:23:40 +0800

    Update index.d.ts

--- @types/tsoa/index.d.ts
+++ @types/tsoa/index.d.ts
@@ -1,21 +1,21 @@
 // Global TSOA Request interface extension
 declare global {
-  namespace Tsoa {
-    interface Request<T = any> {
-      context?: T;
-      requestId?: string;
-      user?: T;
+    namespace Tsoa {
+        interface Request<T = any> {
+            context?: T;
+            requestId?: string;
+            user?: T;
+        }
     }
-  }
 }
 
 // Re-export TSOA types with extensions
 declare module '@tsoa/runtime' {
-  interface Request<T = any> {
-    context?: T;
-    requestId?: string;
-    user?: T;
-  }
+    interface Request<T = any> {
+        context?: T;
+        requestId?: string;
+        user?: T;
+    }
 }
 
 export {};
