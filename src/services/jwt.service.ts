1be62d7f514929e876b23b9d571e86465210be90
JL <johnliveeoroncillo@gmail.com>
2026-03-11 16:14:20 +0800

    Update jwt.service.ts

--- src/services/jwt.service.ts
+++ src/services/jwt.service.ts
@@ -112,4 +112,27 @@ export class JwtService {
         const cookieValue = `${this.COOKIE_NAME}=; HttpOnly; Expires=Thu, 01 Jan 1970 00:00:00 GMT; Path=/`;
         res.setHeader('Set-Cookie', cookieValue);
     }
+
+    /**
+     * Hash password using bcrypt
+     * @param password Plain text password
+     * @returns Hashed password
+     */
+    static async hashPassword(password: string): Promise<string> {
+        const saltRounds = 12;
+        return bcrypt.hash(password, saltRounds);
+    }
+
+    /**
+     * Compare password with hash
+     * @param password Plain text password
+     * @param hash Hashed password
+     * @returns True if passwords match
+     */
+    static async comparePassword(
+        password: string,
+        hash: string,
+    ): Promise<boolean> {
+        return bcrypt.compare(password, hash);
+    }
 }
